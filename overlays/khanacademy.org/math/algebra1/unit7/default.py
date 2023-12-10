86afec3b1a0c635404f6de886b4689eff7c0a146
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-10 10:38:17 +0000

    other example which is optimal

--- overlays/khanacademy.org/math/algebra1/unit7/default.py
+++ overlays/khanacademy.org/math/algebra1/unit7/default.py
@@ -3,7 +3,7 @@ import numpy as np
 
 x  = cp.Variable()
 y = cp.Variable()
-
+# non solvable
 constraints = [y >= 2*x + 1, 
                y <= 2*x - 5,
                x >= 1]
@@ -15,3 +15,16 @@ problem = cp.Problem(objective, constraints)
 problem.solve()
 
 print(problem.status)
+
+
+constraints = [y >= x - 8, 
+               y <= 5 - x]
+
+objective = cp.Maximize(x)
+
+problem = cp.Problem(objective, constraints)    
+
+problem.solve()
+
+print(problem.status)
+print(problem.value)
