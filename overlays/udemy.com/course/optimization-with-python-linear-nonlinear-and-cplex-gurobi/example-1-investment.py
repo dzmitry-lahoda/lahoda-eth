19291ce8bcae8eb5615ccef9fe77cc07db129e13
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-01-05 14:03:50 +0000

    solved example

---
+++ overlays/udemy.com/course/optimization-with-python-linear-nonlinear-and-cplex-gurobi/example-1-investment.py
@@ -0,0 +1,23 @@
+import cvxpy as cp
+
+capital = cp.Variable(3)
+
+total = 100000
+
+constrains = [
+    sum(capital) == 100000,
+    capital[1] <= 0.2*100000,
+    capital[2] <= 0.1*100000,
+    ]
+
+
+objective = cp.Maximize(0.05*capital[0] + 0.1*capital[1] + 0.12*capital[2])
+
+problem = cp.Problem(objective, constrains)
+
+result = problem.solve()
+
+print(problem.status)
+print(problem.value)
+print(problem)
+print(capital.value)
